071f6cc74b637fb84e53d8481800e83613f24858
Arsénio Muanda <manomuanda@gmail.com>
2024-12-22 14:08:57 +0100

    :white_check_mark: login validation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +19,6 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'arsenio@gmail.com',
-            'password' => 'Angelica12'
         ]);
     }
 }
